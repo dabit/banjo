fdc83d79955155d58955f74846960d0dd9cca8b4
David Padilla <david@crowdint.com>
2013-10-17 14:46:08 -0500

    Most of the implementation

--- lib/banjo.rb
+++ lib/banjo.rb
@@ -1,5 +1,40 @@
-require "banjo/version"
+require 'banjo/version'
+
+require 'eventmachine'
+require 'unimidi'
+
+require 'banjo/channel'
+
+$: << '.'
 
 module Banjo
-  # Your code goes here...
+  class << self
+    attr_accessor :tempo
+  end
+
+  def self.load_channels
+    Dir['./channels/*.rb'].each do |file|
+      load file
+    end
+  end
+
+  def self.play
+    tempo_in_ms = 60.0 / Banjo.tempo / 16
+
+    EventMachine.run do
+      n = 0
+      EM.add_periodic_timer(tempo_in_ms) do
+        begin
+          Banjo.load_channels
+        end
+
+        Banjo::Channel.channels.each do |klass|
+          channel = klass.new(n)
+          channel.perform
+        end
+
+        n < 15 ? n += 1 : n = 0
+      end
+    end
+  end
 end
---
+++ lib/banjo/channel.rb
@@ -0,0 +1,36 @@
+module Banjo
+  class Channel
+    attr_accessor :output, :tick
+
+    def self.channels
+      @channels ||= []
+    end
+
+    def self.inherited(child)
+      channels << child
+    end
+
+    def initialize(tick)
+      @output = UniMIDI::Output.all[channel]
+      @tick = tick
+    end
+
+    def tick_note(tick, note, velocity = 50, duration = 0.5)
+      play_note(note, velocity, duration) if tick == self.tick
+    end
+
+    def mod_note(mod, note, offset = 0, velocity = 50)
+      play_note(note, velocity, 0.5) if ((tick + offset) % mod == 0)
+    end
+
+    def play_note(note, velocity = 50, duration = 0.5)
+      Thread.new do
+        output.open do |o|
+          o.puts(0x80, note, velocity)
+          sleep(duration)
+          o.puts(0x90, note, velocity)
+        end
+      end
+    end
+  end
+end
