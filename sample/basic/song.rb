db6afa2aadd3a26359d1d2e1e48f8e839b6d8a54
David Padilla <david@crowdint.com>
2013-10-18 12:32:03 -0500

    Adapt to latest changes

--- sample/basic/song.rb
+++ sample/basic/song.rb
@@ -2,6 +2,11 @@ require 'bundler'
 Bundler.require
 
 Banjo.load_channels
-Banjo.tempo = 100.0
-Banjo.ticks_per_period = 32
+Banjo.tempo             = 100
+Banjo.ticks_per_beat    = 8.0
+Banjo.beats_per_measure = 4.0
+Banjo.measures_per_loop = 1.0
+Banjo.measures_per_loop = 1.0
+
+
 Banjo.play
