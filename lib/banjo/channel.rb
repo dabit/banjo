c071dd8c8691e9ddfb499938d735a1595843cdba
David Padilla <david@crowdint.com>
2013-10-18 12:26:52 -0500

    Modulation, Pitch and STFU

--- lib/banjo/channel.rb
+++ lib/banjo/channel.rb
@@ -38,6 +38,14 @@ module Banjo
       EM.defer { play_note!(note, velocity, duration) }
     end
 
+    def modulation(value = 0)
+      output.puts 0xB0, 0x01, value
+    end
+
+    def pitch(value = 63)
+      output.puts 0xE0, 0, value
+    end
+
     def arpeggio(notes, velocity = 50)
       step = (1.0 * Banjo.ticks_per_period / notes.size).round
       notes.each_with_index do |note, index|
@@ -45,12 +53,14 @@ module Banjo
       end
     end
 
+    def stfu
+      output.puts 0xB0, 0x7B, 0
+    end
+
     def play_note!(note, velocity = 50, duration = DEFAULT_DURATION)
-      output.open do |o|
-        o.puts(0x80, note, velocity)
-        sleep(duration)
-        o.puts(0x90, note, velocity)
-      end
+      output.puts(0x80, note, velocity)
+      sleep(duration)
+      output.puts(0x90, note, velocity)
     end
   end
 end
