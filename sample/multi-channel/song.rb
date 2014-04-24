4138b4b9d4d73dc5df7613f2a9f4ceabb4a7f67d
David Padilla <david@crowdint.com>
2014-04-24 11:26:53 -0500

    Updated sample with new setup

--- sample/multi-channel/song.rb
+++ sample/multi-channel/song.rb
@@ -1,10 +1,15 @@
 require 'bundler'
 Bundler.setup
 
-
 require 'banjo'
 
-Banjo.load_channels
-Banjo.tempo = 60.0
-Banjo.play
+Banjo.load_channels!
+Banjo::Channel.channels.each do |c|
+  c.new(0).stfu
+end
+Banjo.tempo             = 110
+Banjo.ticks_per_beat    = 8.0
+Banjo.beats_per_measure = 4.0
+Banjo.measures_per_loop = 1.0
 
+Banjo.play
