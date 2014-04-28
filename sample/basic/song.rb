cb6b9b7fe2822e7a752f25f3ca2807abd6b6057d
Gabriella Gergely <gabi@purchasingplatform.com>
2014-04-28 11:00:40 -0500

    Replace basic example with Twinkle, twinkle... song

--- sample/basic/song.rb
+++ sample/basic/song.rb
@@ -5,8 +5,7 @@ Banjo.load_channels
 Banjo.tempo             = 100
 Banjo.ticks_per_beat    = 8.0
 Banjo.beats_per_measure = 4.0
-Banjo.measures_per_loop = 1.0
-Banjo.measures_per_loop = 1.0
+Banjo.measures_per_loop = 4.0
 
 
 Banjo.play
