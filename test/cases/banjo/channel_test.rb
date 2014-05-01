bec5070873292e93a8f7310649516400c4192ea9
David Padilla <david@crowdint.com>
2014-05-01 01:00:17 -0500

    Increase Banjo::Channel test coverage

--- test/cases/banjo/channel_test.rb
+++ test/cases/banjo/channel_test.rb
@@ -1,11 +1,50 @@
 require 'test_helper'
 
 class ChannelTest < Test::Unit::TestCase
-  def subject
-    @subject ||= Banjo::Channel.new
+  def channel
+    @channel ||= Banjo::Channel.new
   end
 
   def test_channel
-    assert_equal 0, subject.channel
+    assert_equal 0, channel.channel
+  end
+
+  def test_play
+    note = channel.play(22)
+    assert_equal 22, note.note
+    assert_equal channel, note.channel
+    assert_kind_of Banjo::Note, note
+  end
+
+  def test_midi_channels
+    assert_equal [0x90, 0x80], channel.midi_channels
+  end
+
+  def test_channels
+    assert_equal [], Banjo::Channel.channels
+  end
+
+  def test_modulation
+    channel.output = stub!
+    mock(channel.output).puts(0xB0, 0x01, 100)
+    channel.modulation(100)
+  end
+
+  def test_pitch
+    channel.output = stub!
+    mock(channel.output).puts(0xE0, 0, 100)
+    channel.pitch(100)
+  end
+
+  def test_sustain
+    channel.output = stub!
+    mock(channel.output).puts(0xB0, 0x40, 100)
+    channel.sustain(100)
+  end
+
+  def test_stfu
+    channel.output = stub!
+    mock(channel.output).puts(0xB0, 0x7B, 0)
+    channel.stfu
   end
 end
